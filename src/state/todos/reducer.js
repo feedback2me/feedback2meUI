cba46d64ad6dd945e2d83831e57e787a9251b881
Burak Buruk <burak.bburuk@gmail.com>
2020-04-12 20:24:42 +0200

    immutable integrated

--- src/state/todos/reducer.js
+++ src/state/todos/reducer.js
@@ -4,14 +4,11 @@ import constants from '../../utils/constants';
 const todos = (state = initialState.todos, action) => {
     switch (action.type) {
     case constants.addTodo:
-        return [
-            ...state,
-            {
-                id: action.id,
-                text: action.text,
-                completed: false,
-            },
-        ];
+        return state.push({
+            id: action.id,
+            text: action.text,
+            completed: false,
+        });
     case constants.toggleTodo:
         return state.map((todo) => (todo.id === action.id ? { ...todo, completed: !todo.completed } : todo));
     default:
