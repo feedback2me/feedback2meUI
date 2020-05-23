0ca7f06e2a6a29b8a442292d3d7a5cd50f336ad3
Burak Buruk <burak.bburuk@gmail.com>
2020-05-23 13:27:41 +0200

    morgan added for request tracing

---
+++ server/src/app-dev.js
@@ -0,0 +1,51 @@
+// eslint-disable import/no-extraneous-dependencies
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import express from 'express';
+import compression from 'compression';
+import helmet from 'helmet';
+import path from 'path';
+import morgan from 'morgan';
+
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from '../config/webpack.dev';
+
+import api from './api';
+import logger from './logger';
+
+const app = express();
+
+const compiler = webpack(config);
+
+// Enable "webpack-dev-middleware"
+app.use(
+    webpackDevMiddleware(compiler, {
+        publicPath: config.output.publicPath,
+    }),
+);
+
+// Enable "webpack-hot-middleware"
+app.use(webpackHotMiddleware(compiler));
+
+// investigate this
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(morgan('tiny'));
+
+api(app);
+
+app.get('/', (req, res) => res.send('Home page'));
+
+const port = process.env.PORT || 8080;
+const ROOT_URL = `http://localhost:${port}`;
+
+app.listen(port, (err) => {
+    if (err) throw err;
+    logger.info(`> Ready on ${ROOT_URL}`);
+});
