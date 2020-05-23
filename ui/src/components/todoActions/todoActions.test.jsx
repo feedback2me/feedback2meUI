b51392c7cf487d7f5bf57d8d04994c8e2526e61f
Burak Buruk <burak.bburuk@gmail.com>
2020-05-23 14:16:13 +0200

    gitignore changed

--- ui/src/components/todoActions/todoActions.test.jsx
+++ ui/src/components/todoActions/todoActions.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Footer from '.';
+import TodoActions from '.';
 
 describe('Given Footer', () => {
-    const wrapper = shallow(<Footer />);
+    const wrapper = shallow(<TodoActions />);
+
     it('should render correctly', () => {
         expect(wrapper).toMatchSnapshot();
     });
