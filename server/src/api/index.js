bb050cdca2514dade5a24e1bb59a59664c832a5b
Burak Buruk <burak.bburuk@gmail.com>
2020-08-14 19:44:42 +0200

    basic authentication implemented

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,7 +1,34 @@
 const helloWorldApi = require('./helloWorld');
+const AuthorizationRouter = require('./authorization/routes.config');
+const UsersRouter = require('./users/routes.config');
+
+function setHeaders(server) {
+    return server.use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header(
+            'Access-Control-Allow-Methods',
+            'GET,HEAD,PUT,PATCH,POST,DELETE',
+        );
+        res.header('Access-Control-Expose-Headers', 'Content-Length');
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Accept, Authorization, Content-Type, X-Requested-With, Range',
+        );
+        if (req.method === 'OPTIONS') {
+            return res.send(200);
+        } else {
+            return next();
+        }
+    });
+}
 
 function api(server) {
+    setHeaders(server);
+
     server.use('/api/v1/hello-world', helloWorldApi);
+    AuthorizationRouter.routesConfig(server);
+    UsersRouter.routesConfig(server);
 }
 
 module.exports = api;
